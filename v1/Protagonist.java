d223d56f642164b5eff07262eb044eed5cad08a7
Kevin Li <58864927+KevinLi0711@users.noreply.github.com>
2021-11-23 12:08:50 -0500

    Update Protagonist.java

--- v1/Protagonist.java
+++ v1/Protagonist.java
@@ -6,10 +6,6 @@ public class Protagonist extends Character{
 
     // ~~~~~~~~~~~ INSTANCE VARIABLES ~~~~~~~~~~~
     private String _name = "J. Doe";
-    private int _hitPts;
-    private int _strength;
-    private int _defense;
-    private double _attack;
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -32,56 +28,15 @@ public class Protagonist extends Character{
       post: initializes instance vars. _name is set to input String.
       **/
     public Protagonist( String name ) {
-        this();
+        //this();
+        super(125, 100, 40, .4)
         _name = name;
     }
 
 
     // ~~~~~~~~~~~~~~ ACCESSORS ~~~~~~~~~~~~~~~~~
     public String getName() { return _name; }
-
-    public int getDefense() { return _defense; }
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-    /**
-      boolean isAlive() -- tell whether I am alive
-      post: returns boolean indicated alive or dead
-      **/
-    public boolean isAlive() {
-        return _hitPts > 0;
-    }
-
-
-    /**
-      int attack(Monster) -- simulates attack on a Monster
-      pre:  Input not null
-      post: Calculates damage to be inflicted, flooring at 0.
-      Calls opponent's lowerHP() method to inflict damage.
-      Returns damage dealt.
-      **/
-    public int attack( Monster opponent ) {
-
-        int damage = (int)( (_strength * _attack) - opponent.getDefense() );
-        //System.out.println( "\t\t**DIAG** damage: " + damage );
-
-        if ( damage < 0 )
-            damage = 0;
-
-        opponent.lowerHP( damage );
-
-        return damage;
-    }//end attack
-
-
-    /**
-      void lowerHP(int) -- lowers life by input value
-      pre:  Input >= 0
-      post: Life instance var is lowered by input ammount.
-      **/
-    public void lowerHP( int damageInflicted ) {
-        _hitPts = _hitPts - damageInflicted;
-    }
 
 
     //prepare a Protagonist for a special attack
